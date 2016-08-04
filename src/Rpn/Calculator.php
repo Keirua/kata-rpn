672d46ec10774b5b2d8fe49a0d37f8ae061195e1
clement <clement@evaneos.com>
2016-08-04 16:01:19 +0200

    Added computation logic

--- src/Rpn/Calculator.php
+++ src/Rpn/Calculator.php
@@ -4,6 +4,48 @@ namespace Rpn;
 
 class Calculator {
     public function compute ($operation){
-        
+        $array = explode(' ', $operation);
+        $stack = [];
+
+        while (count($array) != 0)
+        {
+            $currValue = array_shift($array);
+
+            $resultValue  = $currValue;
+
+            if (!is_numeric($currValue)){
+                $value2 = array_pop($stack);
+                $value1 = array_pop($stack);
+
+                $resultValue = $this->computeOperationResult($value1, $value2, $currValue);
+            }
+
+            array_push($stack, $resultValue);
+        }
+
+        return array_pop ($stack);
+    }
+
+    private function computeOperationResult($value1, $value2, $operator){
+        switch($operator)
+        {
+            case '+':
+                $resultValue = $value1 + $value2;
+                break;
+            case '-':
+                $resultValue = $value1 - $value2;
+                break;
+            case 'x':
+            case '*':
+                $resultValue = $value1 * $value2;
+                break;
+            case '/':
+                $resultValue = $value1 / $value2;
+                break;
+            default:
+                throw new \LogicException(sprintf("%s is not a supported operator", $operator));
+                break;
+        }
+        return $resultValue;
     }
 }
--- tests/Rpn/CalculatorTest.php
+++ tests/Rpn/CalculatorTest.php
@@ -4,12 +4,79 @@ use Rpn\Calculator;
 
 class RpnCalculatorTest extends PHPUnit_Framework_TestCase
 {
-    public function testAdd()
-    {
-        $calculator = new Calculator;
+    private $calculator;
 
-        $result = $calculator->compute("2 3 +");
+    public function __construct(){
+        $this->calculator = new Calculator();
+    }
+
+    public function testAdditionIsImplemented()
+    {
+        $result = $this->calculator->compute("2 3 +");
 
         $this->assertEquals(5, $result);
+    }
+
+    public function testSubstractionIsImplemented()
+    {
+        $result = $this->calculator->compute("2 3 -");
+
+        $this->assertEquals(-1, $result);
+    }
+    
+    public function testDivisionIsImplemented()
+    {
+        $result = $this->calculator->compute("6 2 /");
+
+        $this->assertEquals(3, $result);
+    }
+
+    public function testMultiplicationIsImplemented()
+    {
+        $result = $this->calculator->compute("6 2 *");
+
+        $this->assertEquals(12, $result);
+    }
+
+    public function testInvalidOpertorThrowsException()
+    {
+        $this->expectException(\LogicException::class);
+
+        $result = $this->calculator->compute("6 2 pouet");
+    }
+
+    public function testComputationWith2OperatorsAreSupporeted()
+    {
+        $result = $this->calculator->compute("7 5 2 - +");
+
+        $this->assertEquals(10, $result);
+    }
+
+    public function testComputationWith3OperatorsAreSupporeted()
+    {
+        $result = $this->calculator->compute("3 5 8 * 7 + *");
+
+        $this->assertEquals(141, $result);
+    }
+
+    public function testSampleComputationWorksAsExpected()
+    {
+        $result = $this->calculator->compute("3 4 2 1 + * + 2 /");
+
+        $this->assertEquals(7.5, $result);
+    }
+
+    public function testSampleComputationWithBasicOperatorsIsSupporeted()
+    {
+        $result = $this->calculator->compute("1 2 + 4 * 5 + 3 -");
+
+        $this->assertEquals(14, $result);
+    }
+
+    public function testSampleComputationWithBasicOperators2IsSupporeted()
+    {
+        $result = $this->calculator->compute("5 4 1 2 + * +");
+
+        $this->assertEquals(17, $result);
     }
 }
